de4078168429703705da3780d77892d352a229b7
Buzzactaxe <valiantsimpson@gmail.com>
2018-04-28 12:07:54 +0200

    Udate to Word method integrating audio files

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,21 +4,24 @@ public class Word {
 
     private String mMiwokeTranslation;
     private String mEnglishTranslation;
+    private int mAudioRecoureId;
     private int mImageResource = NO_IMAGE_PROVIDED;
 
     private static final int NO_IMAGE_PROVIDED = -1;
 
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int audioRecoureId) {
 
         mEnglishTranslation = defaultTranslation;
         mMiwokeTranslation = miwokTranslation;
+        mAudioRecoureId = audioRecoureId;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation, int imageView) {
+    public Word(String defaultTranslation, String miwokTranslation, int imageView, int audioResourceId) {
 
         mEnglishTranslation = defaultTranslation;
         mMiwokeTranslation = miwokTranslation;
         mImageResource = imageView;
+        mAudioRecoureId = audioResourceId;
     }
 
     public String getDefaultTranslation(){
@@ -44,6 +47,11 @@ public class Word {
 
         return mImageResource != NO_IMAGE_PROVIDED;
 
+    }
+
+    public int getAudioResourceId() {
+
+        return mAudioRecoureId;
     }
 
 }
